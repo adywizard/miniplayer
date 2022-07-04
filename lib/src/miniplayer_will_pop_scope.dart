fd4ef5426886815c27f7819308946a5f81803dfd
ady wizard <ady.wizard@gmail.com>
2022-07-04 19:28:22 +0200

    new fixes

--- lib/src/miniplayer_will_pop_scope.dart
+++ lib/src/miniplayer_will_pop_scope.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'
+    show BuildContext, Key, ModalRoute, State, StatefulWidget, Widget, WillPopCallback;
 
 class MiniplayerWillPopScope extends StatefulWidget {
   const MiniplayerWillPopScope({
